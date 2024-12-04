e4f48e06ceb3f2601da2aef56265b0829c06098e
IchemJ <ichem76610@hotmail.com>
2024-12-04 15:22:40 +0100

    exercice (leçon javascript)

---
+++ JSpart1/js/index.js
@@ -0,0 +1,50 @@
+// Exercice 1 :
+// let message = "salut";
+// alert (message);
+
+// Exercice 2 :
+// let partOne = "Hello"
+// let partTwo = "World"
+// alert (partOne + " " + partTwo)
+
+// Exercice 3: 
+// let firstname = "ichem"
+// let age = 29
+// let city = "Havre"
+// alert ("Je m'apelle " + firstname + " ,j'ai " + age + " ans" + " et j'habite au " + city)
+
+// deuxieme ecriture avec `${variable}` :
+// alert (`Je m'apelle ${firstname} ,j'ai ${age} ans et j'habite au ${city}`)
+
+// document.write("<p>Le type de firstname est " + typeof firstname + " , " + "Le type de age est " + typeof age + " , " + "Le type de city est " + typeof city + "</p>");
+// console.log("Le type de firstname est " + typeof firstname + " , " + "Le type de age est " + typeof age + " , " + "Le type de city est " + typeof city);
+
+// autoriser variable apres "," en lui donnant une valeur : 
+// let message = "", nom = "", prenom = ""; 
+
+// message = prompt ("donne moi un message ZABI !!!!")
+// nom = prompt ("donne ton nom ZABI !!!")
+// prenom = prompt ("donne ton prenom aussi zabi !!!")
+
+// alert (message + " " + nom + " " + prenom )
+
+// Exercice 4 :
+// let nom, prenom, age, mail;
+// nom = prompt ("Donnez moi votre nom")
+// prenom = prompt ("Donnez moi votre prénom")
+// age = prompt ("Donnez moi votre age")
+// mail = prompt ("Donnez moi votre e-mail")
+
+// alert(`Nom : ${nom} \nPrénom : ${prenom} \nAge : ${age} \nAdresse mail : ${mail}`)
+
+// Exercice 5 : 
+// let nbreFirst
+// let nbreSecond 
+// let resultat
+
+// nbreFirst = parseInt (prompt ("Donnez moi un nombre"))
+// nbreSecond = parseInt (prompt ("Donnez moi un deuxieme nombre"))
+
+// resultat = nbreFirst + nbreSecond
+// alert ( nbreFirst + " + " + nbreSecond + " = " + resultat)
+
---
+++ JSpart2/js/index.js
@@ -0,0 +1,151 @@
+// Exercice 1 :
+// let age 
+// age = parseInt (prompt ("Entrez votre age ?")); 
+
+// if (age >= 18)
+//     alert ("**L'utilisateur est majeur !**");
+// else 
+// alert ("**L'utilisateur est mineur !**");
+
+// Exercice 2: 
+// let nbreEntier
+// nbreEntier = parseInt(prompt("Entrez un nombre entier"));
+
+// if (nbreEntier % 2 == 0) {
+//     alert("Le nombre saisie est un nombre **PAIR**")
+// }
+// else {
+//     alert("le nombre saisie est un nombre **IMPAIR**")
+// }
+
+// Excercice 3:
+// let password = "Secret"
+
+// password = prompt ("Veuillez entrer votre mot de passe d'identification")
+
+// if (password=="Secret") {
+//     alert ("Hello Mr Anderson");
+// }
+// else {
+//     alert("Mot de passe incorrect");
+// }
+
+// Exercice 4:
+// let sex
+// let age 
+// let homme
+
+
+// sex = prompt ("Quel est votre sexe")
+// age = prompt ("Quel age avez vous ?")
+
+// if (sex == "homme") {
+//     homme = true;
+// }
+// else {
+//     homme = false;
+// }
+
+// if (homme & age > 18) {
+//     alert ("L'utilisateur est de type masculin et majeur");
+// }
+// else if (homme & age < 18) {
+//     alert ("L'utilisateur est de type masculin et mineur");
+// }
+// else if (homme==false & age > 18) {
+//     alert ("L'utilisateur est de type feminin et majeur");
+// }
+// else if (homme==false & age < 18 ) {
+//     alert ("L'utilisateur est de type feminin et mineur");
+// }
+
+// Exercice 5:
+// let moyenne
+
+// moyenne = prompt ("Quel est votre moyenne ?")
+
+// if (moyenne==0) {
+//     alert ("Un rendez vous seras programmé pour cette note de merde")
+// }
+// else if (moyenne > 0 & moyenne <= 5) {
+//     alert ("Trimestre insuffisant.")
+// }
+// else if (moyenne > 5 & moyenne <= 10) {
+//     alert ("Trimestre moyen.")
+// }
+// else if (moyenne > 10 & moyenne <= 13) {
+//     alert ("Bon trimestre.")
+// }
+// else if (moyenne > 13 & moyenne <= 18) {
+//     alert ("Excellent trimestre.")
+// }
+// else if (moyenne > 18 & moyenne <= 20) {
+//     alert ("Bsarteck trimestre.")
+// }
+// else if (moyenne > 20) {
+//     alert ("MENTEUR !")
+// }
+
+// Exercice 6:
+// let mois
+
+// mois = prompt("Entre un numero de mois entre 1 et 12 ?")
+
+//     switch (mois) {
+//         case "1":
+//             alert("Le numéro 1 correspond au mois de janvier");
+//             break;
+//         case "2":
+//             alert("Le numéro 2 correspond au mois de février");
+//             break;
+//         case "3":
+//             alert("Le numéro 3 correspond au mois de mars");
+//             break;
+//         case "4":
+//             alert("Le numéro 4 correspond au mois de avril");
+//             break;
+//         case "5":
+//             alert("Le numéro 5 correspond au mois de mai");
+//             break;
+//         case "6":
+//             alert("Le numéro 6 correspond au mois de juin");
+//             break;
+//         case "7":
+//             alert("Le numéro 7 correspond au mois de juillet");
+//             break;
+//         case "8":
+//             alert("Le numéro 8 correspond au mois de aôut");
+//             break;
+//         case "9":
+//             alert("Le numéro 9 correspond au mois de septembre");
+//             break;
+//         case "10":
+//             alert("Le numéro 10 correspond au mois de octobre");
+//             break;
+//         case "11":
+//             alert("Le numéro 11 correspond au mois de novembre");
+//             break;
+//         case "12":
+//             alert("Le numéro 12 correspond au mois de décembre");
+//             break;
+//         default:
+//             alert("Le numéro ne correspond a aucun mois");
+//             break;
+//     }
+
+// Exercice 7:
+// let nbreEntier
+
+// nbreEntier = parseInt(prompt("Entrez un nombre entier"))
+// nbreEntier % 2 == 0
+//     ? alert("Le nombre saisie est un nombre **PAIR**")
+//     : alert("Le nombre saisie est un nombre **IMPAIR**")
+
+// Exercice 8:
+
+// if (window.confirm("En cliquant sur OK, je confirme que je rentre sur ce site en 'mon âme et conscience")) {
+//     alert ("L'utilisateur a cliqué OK")
+// }
+// else {
+//     alert ("L'utilisateur n'a pas validé.")
+// }
