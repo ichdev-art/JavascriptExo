5a56758942ff4563bb072179863e87c74280cc01
IchemJ <ichem76610@hotmail.com>
2024-12-04 16:21:37 +0100

    part3

---
+++ JSpart3/js/index.js
@@ -0,0 +1,47 @@
+// Exercice 1:
+// let i = 0
+
+// while (i <= 100){
+//     console.log (i);
+//     i++;
+// }
+
+// Exercice 2:
+// let i = 102
+
+// while (i > 0){
+//     i=i-2;
+//     console.log (i);
+// }
+
+// Exercice 3:
+// let nb1 = 3
+// let nb2 = 6
+
+// while (nb1 + nb2 < 2023){
+//     console.log (nb1+nb2)
+//     nb2 = nb1 + nb2
+// }
+
+// Exercice 4:
+// let nbre = 0
+// let message = "Message"
+
+// while (nbre < 10){
+//     console.log (message + " " + nbre);
+//     nbre ++
+// }
+// if (nbre == 10){
+//     console.log (message + " " + nbre + " C'est le dernier message !")
+// }
+
+// let nbre = 0
+// let message = "Message"
+
+// while (nbre < 10) {
+//     nbre++
+//     nbre == 10
+//         ? console.log(message + " " + nbre + " C'est le dernier message !")
+//         : console.log(message + " " + nbre);
+// }
+
