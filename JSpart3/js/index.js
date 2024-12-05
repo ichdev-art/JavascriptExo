7401ae4c5d462a7bcfcd2e03d1b14e909291d97b
Ich-dev <ichem76610@hotmail.com>
2024-12-05 12:42:17 +0100

    reste exercice fini

--- JSpart3/js/index.js
+++ JSpart3/js/index.js
@@ -45,3 +45,79 @@
 //         : console.log(message + " " + nbre);
 // }
 
+// Exercice 5 :
+// let message = "Vendredi 13"
+// let nbreRepetition = 1
+// while :
+// while (nbreRepetition <14) {
+//     console.log (message);
+//     nbreRepetition ++;
+// }
+// for :
+// for (let nbreRepetition = 1; nbreRepetition < 14; nbreRepetition ++) {
+//     console.log(message);
+// }
+
+// Exercice 6 :
+// for (let i = 0; i < 99; i=i+3 ) {
+//     console.log (i);
+// }
+
+// Exercice 7 : 
+// for (let i = 20; i >=0; i = i - 1){
+//    if (i == 20){
+//         console.log("Start !!");
+//     }
+//   console.log(i);
+//   if (i==0){
+//     console.log("Finish !!");
+//   }
+// }
+
+// Exercice 8 :
+// let i = 0
+// // while 
+// while (i <= 99) {
+//     i++;
+//     if ((i % 3 == 0 & i % 5 == 0)) {
+//         console.log("Fizz Buzz");
+//     }
+//     else if (i % 3 == 0) {
+//         console.log("Fizz")
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// for 
+// for (let i = 1; i <= 99; i++ ) {
+
+//     if ((i % 3 == 0 & i % 5 == 0)) {
+//                 console.log("Fizz Buzz");
+//             }
+//             else if (i % 3 == 0) {
+//                 console.log("Fizz")
+//             }
+//             else if (i % 5 == 0) {
+//                 console.log("Buzz");
+//             }
+//             else {
+//                 console.log(i);
+//             }
+//         }
+
+// Exercice 9 :
+// let nbre = ""
+
+// for (let i = 1; i <= 10; i ++) {
+//     nbre +=i
+//     if (i<10){
+//         nbre +="-"
+//     }
+// }
+// console.log(nbre);
+
