c1b79ea26b878f8317d0661bd8b869b3789521bc
IchemJ <ichem76610@hotmail.com>
2024-12-09 11:40:09 +0100

    Tout fini

---
+++ JSpart4/js/index.js
@@ -0,0 +1,72 @@
+// Exercice 1:
+
+// 1er methode 
+// let mois = ["Janvier ", " Février"," Mars"," Avril"," Mai"," Juin"," Juillet"," Aôut"," Septembre"," Octobre"," Novembre"," Décembre",];
+// // console.log(mois);
+// alert(mois);
+// 2eme methode 
+// let mois = [];
+// mois [1] = "Janvier"
+// mois [2] = "Février"
+// mois [3] = "Mars"
+// mois [4] = "Avril"
+// mois [5] = "Mai"
+// mois [6] = "Juin"
+// mois [7] = "Juillet"
+// mois [8] = "Aôut"
+// mois [9] = "Septembre"
+// mois [10] = "Octobre"
+// mois [11] = "Novembre"
+// mois [12] = "Décembre"
+// console.table(mois)
+
+// Exercice 2:
+// let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
+// console.log (days[4])
+
+// Exercice 3:
+// let days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",];
+// // console.log (days[days.length - 2])
+// console.log (days[5])
+
+// Exercice 4:
+// let mois = ["Janvier ", "Février","Mars","Avril","Mai","Juin","Juillet","Aôut","Septembre","Octobre","Novembre","Décembre",];
+// console.log(mois.length);
+// mois.forEach((item, index) => {
+//     console.log(index+1,item);
+// })
+
+// Exercice 5:
+// let mois = ["Janvier ", "Février","Mars","Avril","Mai","Juin","Juillet","Aôut","Septembre","Octobre","Novembre","Décembre",];
+// let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",];
+
+// let month = ["January ", "Febuary","March","April","May","June","July","August","September","October","November","December",];
+// let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
+
+// let fr = [mois, jours]
+// let en = [month,days]
+
+// let tab = [fr,en]
+// console.log (mois[10]+" - "+days[4])
+// console.log (month)
+// console.log (jours)
+
+// Exercice 6:
+// let pdisney = ["Cendrillon"]
+
+// console.log (pdisney)
+
+// let princess = pdisney.push("Mulan", "Peach", "Raiponce", "Belle", "Vaiana", "Zelda", "Jasmine")
+
+// console.log (pdisney)
+
+// pdisney.splice(2,1)
+// pdisney.splice(5,1)
+
+
+// console.log(pdisney)
+
+// pdisney.sort();
+// console.log(pdisney)
+
+// console.log("Il y a "+ pdisney.length + " princesse dans le tableau")
